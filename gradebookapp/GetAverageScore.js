52aff83683d5dc6cd95deb174ffd25286d73a2ba
Ghost9841 <asubedi778@gmail.com>
2025-12-27 09:45:54 +0545

    this one was quite easy

--- gradebookapp/GetAverageScore.js
+++ gradebookapp/GetAverageScore.js
@@ -28,3 +28,5 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+// Go to GetGradeScore.js for Step 2;
---
+++ gradebookapp/GetGradeScore.js
@@ -0,0 +1,50 @@
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+// Step 2
+// Now the teacher needs your help converting the student score to a letter grade.
+// 
+// Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+// 
+// Here are the scores and their corresponding letter grades:
+// 
+// Score Range	Grade
+// 100	"A++"
+// 90 - 99	"A"
+// 80 - 89	"B"
+// 70 - 79	"C"
+// 60 - 69	"D"
+// 0 - 59	"F"
+// Tips
+// 
+// Remember that you learned about conditional statements (if, else if, and else).
+// Remember that you learned about comparison operators (>, <, >=, <=, ===).
+// Remember that you learned about the logical AND operator (&&).
+
+function getGrade(score) {
+  if (score === 100){
+    return "A++"
+  } else if (score >= 90 && score <= 99){
+    return "A"
+  }else if (score >= 80 && score <= 89){
+    return "B"
+  }
+  else if (score >= 70 && score <= 79){
+    return "C"
+  }else if (score >= 60 && score <= 69){
+    return "D"
+  }else if (score >= 0 && score <= 59){
+    return "F"
+  }
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
